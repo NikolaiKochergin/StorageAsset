1bd38b6ec00073bf27473c1e7f357301a8c1d810
Nikolai Kochergin <Kocherginizer@gmail.com>
2022-11-23 16:07:19 +0300

    Update Game Analytics

--- Assets/Source/Scripts/Analytics/AnalyticManager.cs
+++ Assets/Source/Scripts/Analytics/AnalyticManager.cs
@@ -1,53 +1,123 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AnalyticManager : MonoBehaviour
+#if GAME_ANALYTICS
+using GameAnalyticsSDK;
+#endif
+
+namespace Source.Scripts.Analytics
 {
-    //
-    // public void SendEventOnGameInitialize(int sessionCount)
-    // {
-    //     _appMetricaObject.OnGameInitialize(sessionCount);
-    //     _gameAnalyticsObject.OnGameInitialize(sessionCount);
-    // }
-    //
-    // public void SendEventOnLevelStart(int levelNumber)
-    // {
-    //     _appMetricaObject.OnLevelStart(levelNumber);
-    //     _gameAnalyticsObject.OnLevelStart(levelNumber);
-    // }
-    //
-    // public void SendEventOnLevelComplete(int levelNumber)
-    // {
-    //     _appMetricaObject?.OnLevelComplete(levelNumber);
-    //     _gameAnalyticsObject?.OnLevelComplete(levelNumber);
-    // }
-    //
-    // public void SendEventOnFail(int levelNumber)
-    // {
-    //     _appMetricaObject?.OnFail(levelNumber);
-    //     _gameAnalyticsObject?.OnFail(levelNumber);
-    // }
-    //
-    // public void SendEventOnLevelRestart(int levelNumber)
-    // {
-    //     _appMetricaObject?.OnLevelRestart(levelNumber);
-    //     _gameAnalyticsObject?.OnLevelRestart(levelNumber);
-    // }
-    //
-    // public void SendEventOnSoftSpend(string purchaseType, string storeName, int purchaseAmount, int purchasesCount)
-    // {
-    //     _appMetricaObject.OnSoftSpend(purchaseType, storeName, purchaseAmount, purchasesCount);
-    //     _gameAnalyticsObject.OnSoftSpend(purchaseType, storeName, purchaseAmount, purchasesCount);
-    // }
-    //
-    // public void SendEventOnGameExit(string registrationDate, int sessionCount, int daysInGame)
-    // {
-    //     _appMetricaObject.OnGameExit(registrationDate, sessionCount, daysInGame);
-    //     _gameAnalyticsObject.OnGameExit(registrationDate, sessionCount, daysInGame);
-    // }
-    //
-    // public void SendEventOnGameExit(string registrationDate, int sessionCount, int daysInGame, int currentSoft)
-    // {
-    //     _appMetricaObject.OnGameExit(registrationDate, sessionCount, daysInGame, currentSoft);
-    //     _gameAnalyticsObject.OnGameExit(registrationDate, sessionCount, daysInGame, currentSoft);
-    // }
+    public class AnalyticManager
+    {
+        public AnalyticManager()
+        {
+#if GAME_ANALYTICS
+            GameAnalytics.Initialize();
+#endif
+        }
+
+        public void SendEventOnGameInitialize(int sessionCount)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Count, sessionCount}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, AnalyticNames.GameStart, obj);
+#endif
+        }
+
+        public void SendEventOnLevelStart(int levelNumber)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Level, levelNumber}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, AnalyticNames.LevelStart, obj);
+#endif
+        }
+
+        public void SendEventOnLevelComplete(int levelNumber)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Level, levelNumber},
+                {AnalyticNames.TimeSpent, (int)Time.timeSinceLevelLoad}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, AnalyticNames.LevelComplete, obj);
+#endif
+        }
+
+        public void SendEventOnFail(int levelNumber)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Level, levelNumber},
+                {AnalyticNames.TimeSpent, (int)Time.timeSinceLevelLoad}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Fail, AnalyticNames.Fail, obj);
+#endif
+        }
+
+        public void SendEventOnLevelRestart(int levelNumber)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Level, levelNumber}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, AnalyticNames.Restart, obj);
+#endif
+        }
+
+        public void SendEventOnSoftSpend(string purchaseType, string storeName, int purchaseAmount, int purchasesCount)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Type, purchaseType},
+                {AnalyticNames.Name, storeName},
+                {AnalyticNames.Amount, purchaseAmount},
+                {AnalyticNames.Count, purchasesCount}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, AnalyticNames.SoftSpent, obj);
+#endif
+        }
+
+        public void SendEventOnGameExit(string registrationDate, int sessionCount, int daysInGame)
+        {
+            var regDayObj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Date, registrationDate}
+            };
+            var sessionCountObj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Count, sessionCount}
+            };
+            var daysInGameObj = new Dictionary<string, object>
+            {
+                {AnalyticNames.Day, daysInGame}
+            };
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, AnalyticNames.RegistrationDay, regDayObj);
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, AnalyticNames.SessionCount, sessionCountObj);
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, AnalyticNames.DaysInGame, daysInGameObj);
+#endif
+        }
+
+        public void SendEventOnGameExit(string registrationDate, int sessionCount, int daysInGame, int currentSoft)
+        {
+            var obj = new Dictionary<string, object>
+            {
+                {AnalyticNames.CurrentSoft, currentSoft}
+            };
+            SendEventOnGameExit(registrationDate, sessionCount, daysInGame);
+#if GAME_ANALYTICS
+            GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, AnalyticNames.CurrentSoft, obj);
+#endif
+        }
+    }
 }
--- Assets/Source/Scripts/Analytics/AnalyticNames.cs
+++ Assets/Source/Scripts/Analytics/AnalyticNames.cs
@@ -1,11 +1,24 @@
-public class AnalyticNames
+namespace Source.Scripts.Analytics
 {
-    public const string GameStart = "game_start";
-    public const string LevelStart = "level_start";
-    public const string LevelComplete = "level_complete";
-    public const string Fail = "fail";
-    public const string Restart = "restart";
-    public const string SoftSpent = "soft_spent";
-    public const string RegistrationDay = "reg_day";
-    public const string SessionCount = "session_count";
+    public static class AnalyticNames
+    {
+        public const string GameStart = "game_start";
+        public const string LevelStart = "level_start";
+        public const string LevelComplete = "level_complete";
+        public const string Fail = "fail";
+        public const string Restart = "restart";
+        public const string SoftSpent = "soft_spent";
+        public const string RegistrationDay = "reg_day";
+        public const string SessionCount = "session_count";
+        public const string DaysInGame = "days_in_game";
+        public const string Day = "day";
+        public const string Date = "date";
+        public const string Level = "level";
+        public const string Count = "count";
+        public const string Type = "type";
+        public const string Name = "name";
+        public const string Amount = "amount";
+        public const string CurrentSoft = "current_soft";
+        public const string TimeSpent = "time_spent";
+    }
 }
